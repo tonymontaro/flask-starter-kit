58136f3bc02e00d36e0a533dad63f0b1ee6d8718
Anthony Ngene <ngeneanthony@gmail.com>
2018-06-16 12:04:12 +0100

    (feature): add option for logged-in user to delete his/her account

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,20 +1,18 @@
 """Sample API routes."""
-from flask_restful import Resource
+from flask import Blueprint, jsonify
 from flask_login import login_required
 
-
-class Home(Resource):
-    """Sample home resource."""
-
-    def get(self):
-        """Welcome page/message."""
-        return {'message': 'Hello World!'}
+api_bp = Blueprint('api', __name__)
 
 
-class Protected(Resource):
-    """Sample protected resource."""
+@api_bp.route('/', methods=['GET'])
+def home():
+    """Welcome page/message."""
+    return jsonify({'message': 'Hello World!'})
 
-    @login_required
-    def get(self):
-        """A protected route."""
-        return {'message': 'You are logged-in.'}
+
+@api_bp.route('/protected', methods=['GET'])
+@login_required
+def protected():
+    """A protected route."""
+    return jsonify({'message': 'You are logged-in.'})
--- makefile
+++ makefile
@@ -1,7 +1,9 @@
 install:
 	cp env_sample .env
 	pip install -r requirements.txt
-	flask db init
-	flask db migrate
-	flask db upgrade
-
+	flask db init && flask db migrate && flask db upgrade
+update_db:
+	flask db migrate && flask db upgrade
+new_db:
+	rm -rf migrations
+	flask db init && flask db migrate && flask db upgrade
